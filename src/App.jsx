7be1d6f386a4ce999f1c41249e31c0460c4be67d
Aniket Patel <patelaniket1207@gmail.com>
2023-05-09 20:55:48 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <>
+    {/* This is just demo texts and styles, purpose is to finalize folder structure and components. */}
+     <h1>The Serene Resort</h1>
+     <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../assets/css/Navbar.css";
+
+const Navbar = () =>{
+    return(
+        <>
+        <div className="nav-container"> 
+            Navbar container
+        </div>
+        </>
+    )
+}
+
+export default Navbar;
