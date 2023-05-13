41de1ac2ea192ca55398b11c3bc25a0b47653bb0
Aniket Patel <patelaniket1207@gmail.com>
2023-05-13 17:56:01 +0530

    Added homepage

--- src/Components/HeroContainer.jsx
+++ src/Components/HeroContainer.jsx
@@ -1,16 +1,35 @@
 import React from "react";
-import '../assets/css/HeroContainer.css';
+import "../assets/css/HeroContainer.css";
+import img from "../assets/Images/hero-image.png";
 
-const HeroContainer = () =>{
-    return(
-        <>
-            <div className="hero-container">
-                <p>
-                    Welcome to serene Resort (Hero section)
-                </p>
-            </div>
-        </>
-    )
-}
+const HeroContainer = () => {
+  return (
+    <>
+      <div className="hero-container">
+        <div className="Main-section">
+          <div className="main-texts">
+            <p className="tagline">
+              Dream Homes in <br /> Paradise from <br /> €65,000
+            </p>
+            <p className="basic-info-text">
+              Discover the paradise island of Sumba, Indonesia, located just one
+              hour away from Bali, and become a citizen and investor of Eco
+              Beach City.
+            </p>
+            <section className="button-section">
+              <button>
+                <p>Get More Info</p>
+              </button>
+            </section>
+          </div>
+          <div className="main-image">
+            <img src={img} alt="hero" />
+            {/* <img src="https://pokemonletsgo.pokemon.com/assets/img/common/char-pikachu.png" alt="hero" /> */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default HeroContainer;
