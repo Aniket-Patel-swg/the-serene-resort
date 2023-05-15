f269fd257ec418c25d83b99d39f4219a69bbcb34
Reinaldocoronel <reinaldocoronel1708@gmail.com>
2023-05-15 14:53:59 -0400

    adding NavBar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,16 @@ const Navbar = () =>{
     return(
         <>
         <div className="nav-container"> 
-            Navbar container
+        <header sticky="top">
+            <img src="https://www.ecobeachcity.com/wp-content/uploads/2022/09/Logo-new.png" alt="Eco Beach City Logo" />
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">About Us</a>
+                <a href="#">Services</a>
+                <a href="#">Contact</a>
+            </nav>
+            <a href="#" class="cta-button">Call to Action</a>
+        </header>
         </div>
         </>
     )
