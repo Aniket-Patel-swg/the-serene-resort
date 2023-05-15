eebf98ee4ba370bbea55f7547188c9ac38da01e7
Aniket Patel <patelaniket1207@gmail.com>
2023-05-15 12:19:41 +0530

    Added Scroll to top functionality

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -12,11 +12,24 @@ import Investment from "../Components/Investment";
 import AboutUs from "../Components/AboutUs";
 import Team from "../Components/Team";
 import FinalCallToAction from "../Components/FinalCallToAction";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import '../assets/CSS/HomePage.css';
 
 const HomePage = () => {
+  const handleClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
   return (
     <>
       <div className="home-page">
+        <section className="scroll-button">
+          <div className="back-to-top-btn" onClick={handleClick}>
+            <ArrowDropUpIcon fontSize="large" />
+          </div>
+        </section>
         <HeroContainer />
         <Refrences />
         <Carousel />
