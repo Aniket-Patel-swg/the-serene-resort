d6206f1f1c1c9b666e98df2d54c2af707e430c54
Aniket Patel <patelaniket1207@gmail.com>
2023-06-05 00:02:53 +0530

    Added ttf folder in /public for kollketif font

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import HomePage from './Pages/HomePage';
+import './font.css'
 
 function App() {
   return (
