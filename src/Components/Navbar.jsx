028b4def4003ae249735e448719ecb4354ed52b4
Aniket Patel <patelaniket1207@gmail.com>
2023-06-07 21:18:23 +0530

    Added simple logic for hamburger navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,50 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/css/Navbar.css";
+import { FaBars } from "react-icons/fa";
+import {IoMdClose} from "react-icons/io"
 
 const Navbar = ({ about, services, contact, cta }) => {
-    const smoothScroll = (e, target) => {
-      e.preventDefault();
-      document.querySelector(target).scrollIntoView({
-        behavior: "smooth",
-      });
-    };
-  
-    const scrollToHome = () =>{
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-          });
-    }
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
+  const [showHamburger, setShowHamburger] = useState(false);
 
-    return (
-      <>
-        <div className="nav-container">
-          <header sticky="top">
-            <img
-              src="https://www.ecobeachcity.com/wp-content/uploads/2022/09/Logo-new.png"
-              alt="Eco Beach City Logo"
-            />
-            <nav>
-              <a href="#" onClick={scrollToHome}>
-                Home
-              </a>
-              <a href={about} onClick={(e) => smoothScroll(e, "#about")}>
-                About Us
-              </a>
-              <a href={services} onClick={(e) => smoothScroll(e, "#services")}>
-                Services
-              </a>
-              <a href={contact} onClick={(e) => smoothScroll(e, "#contact")}>
-                Contact
-              </a>
-            </nav>
-            <a href={cta} className="cta-button" onClick={(e) => smoothScroll(e, "#calltoaction")}>
-              Call to Action
-            </a>
-          </header>
-        </div>
-      </>
-    );
+  const smoothScroll = (e, target) => {
+    e.preventDefault();
+    document.querySelector(target).scrollIntoView({
+      behavior: "smooth",
+    });
   };
 
-  export default Navbar;
+  const scrollToHome = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <>
+      <div className="nav-container">
+        <header sticky="top">
+          <img
+            src="https://www.ecobeachcity.com/wp-content/uploads/2022/09/Logo-new.png"
+            alt="Eco Beach City Logo"
+          />
+          <nav>
+            <a href="#" onClick={scrollToHome}>
+              Home
+            </a>
+            <a href={about} onClick={(e) => smoothScroll(e, "#about")}>
+              About Us
+            </a>
+            <a href={services} onClick={(e) => smoothScroll(e, "#services")}>
+              Services
+            </a>
+            <a href={contact} onClick={(e) => smoothScroll(e, "#contact")}>
+              Contact
+            </a>
+          </nav>
+          <a
+            href={cta}
+            className="cta-button"
+            onClick={(e) => smoothScroll(e, "#calltoaction")}
+          >
+            Call to Action
+          </a>
+          <section className="hamburger-menu">
+            <a onClick={() => setShowMediaIcons(!showMediaIcons)}>
+              {showHamburger ? (
+                <IoMdClose onClick={() => setShowHamburger(false)} />
+              ) : (
+                <FaBars onClick={() => setShowHamburger(true)} />
+              )}
+            </a>
+          </section>
+          {showMediaIcons && (
+            <>
+              <section className="mobile-nav">
+                <a href="#" onClick={scrollToHome}>
+                  Home
+                </a>
+                <a href={about} onClick={(e) => smoothScroll(e, "#about")}>
+                  About Us
+                </a>
+                <a
+                  href={services}
+                  onClick={(e) => smoothScroll(e, "#services")}
+                >
+                  Services
+                </a>
+                <a href={contact} onClick={(e) => smoothScroll(e, "#contact")}>
+                  Contact
+                </a>
+              </section>
+            </>
+          )}
+        </header>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
