177e17971206566143747578d83a96e8c808e45c
Aniket Patel <patelaniket1207@gmail.com>
2023-06-07 22:49:57 +0530

    added whatsapp link for call to action section

--- src/Components/FinalCallToAction.jsx
+++ src/Components/FinalCallToAction.jsx
@@ -8,9 +8,12 @@ const FinalCallToAction = () => {
       <div className="final-call-to-action">
         <div className="final-texts">
           <h2>
-            If you have already made up <br /> your mind, don't hesitate to <br /> contact us
+            If you have already made up <br /> your mind, don't hesitate to{" "}
+            <br /> contact us
           </h2>
-          <button>Get More info</button>
+          <a href="https://wa.me/+7990154651" target="_blank">
+            <button>Get More info</button>
+          </a>
         </div>
         <img src={FinalCallToActionimg} alt="" />
       </div>
