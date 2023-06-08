0cd00872169570efc03db3d66ff4983071c6b93b
Aniket Patel <patelaniket1207@gmail.com>
2023-06-08 19:56:42 +0530

    Added fully functional alternate carousel for laptop sizez

--- src/Components/Alternate_Carousel.jsx
+++ src/Components/Alternate_Carousel.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import "../assets/CSS/Alternate_Carousel.css";
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 
 const AlternateCarousel = () => {
-
   const [activeIndex, setActiveIndex] = useState(0);
   const itemHeight = 6;
   const smallTitleHeight = 4;
+  const descriptionHeight = 13.5;
 
   const carousel = [
     {
@@ -14,7 +15,7 @@ const AlternateCarousel = () => {
       title: "The New Bali",
       smalltitle: "Kerela Unique and Exclusive",
       description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga nisi perspiciatis esse. Ab quae vel deserunt eum, dolorum accusantium quo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates modi labore iste! Voluptas.",
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo fuga quisquam distinctio commodi earum similique totam magni doloribus. Nam, laboriosam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, sapiente nostrum quo unde amet accusamus quas nulla aspernatur nihil harum.",
     },
     {
       image:
@@ -22,7 +23,7 @@ const AlternateCarousel = () => {
       title: "Western Comforts in South India",
       smalltitle: "Enjoy Life at Safe Place",
       description:
-        "Lorem, ipsum dolor sit amet . Fuga nisi perspiciatis esse. Ab quae vel deserunt eum, dolorum accu santium quo? Lorem ipsum dolor sit amet consectetur adipisicing  modi labore iste! Voluptas. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga nisi perspiciatis esse",
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo fuga quisquam distinctio commodi earum similique totam magni doloribus. Nam, laboriosam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, sapiente nostrum quo unde amet accusamus quas nulla aspernatur nihil harum.",
     },
     {
       image:
@@ -30,7 +31,7 @@ const AlternateCarousel = () => {
       title: "Discover Serene",
       smalltitle: "The only house you need",
       description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga nisi ,vel deserunt eum, dolorum accusantium quo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates modi labore iste! Voluptas. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga nisi perspiciattas.",
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo fuga quisquam distinctio commodi earum similique totam magni doloribus. Nam, laboriosam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, sapiente nostrum quo unde amet accusamus quas nulla aspernatur nihil harum.",
     },
     // more descriptions...
   ];
@@ -64,46 +65,65 @@ const AlternateCarousel = () => {
           </div>
         </div>
         <div className="text_section">
-            <section className="title_section">
-              <div
-                className="title_wrapper"
-                style={{
-                  transform: `translateY(-${activeIndex * 100}%)`,
-                  height: `${itemHeight}vh`,
-                }}
-              >
-                {carousel.map((item) => {
-                  return (
-                    <>
-                        <h1 className="title">{item.title}</h1>
-                    </>
-                  );
-                })}
-              </div>
-            </section>
-            <section className="small_title_section">
-              <div className="small_title_wrapper"
+          <section className="title_section">
+            <div
+              className="title_wrapper"
               style={{
-                transform : `translateY(-${activeIndex * 100}%)`,
-                height : `${smallTitleHeight}vh`,
+                transform: `translateY(-${activeIndex * 100}%)`,
+                height: `${itemHeight}vh`,
               }}
-              >
-                {carousel.map((item)=>{
-                  return(
-                    <>
-                      <h2 className="small_title">
-                        {item.smalltitle}
-                      </h2>
-                    </>
-                  )
-                })}
-              </div>
-            </section>
-            <section className="description_section">
-              
-            </section>
-            <button onClick={handleLeftButtonClick}>Left</button>
-            <button onClick={handleRightButtonClick}>Right</button>
+            >
+              {carousel.map((item) => {
+                return (
+                  <>
+                    <h1 className="title">{item.title}</h1>
+                  </>
+                );
+              })}
+            </div>
+          </section>
+          <section className="small_title_section">
+            <div
+              className="small_title_wrapper"
+              style={{
+                transform: `translateY(-${activeIndex * 100}%)`,
+                height: `${smallTitleHeight}vh`,
+              }}
+            >
+              {carousel.map((item) => {
+                return (
+                  <>
+                    <h2 className="small_title">{item.smalltitle}</h2>
+                  </>
+                );
+              })}
+            </div>
+          </section>
+          <section className="description_section">
+            <div
+              className="description_wrapper"
+              style={{
+                transform: `translateY(${activeIndex * 103}%)`,
+                height: `${descriptionHeight}vh`,
+              }}
+            >
+              {carousel.map((item) => {
+                return (
+                  <>
+                    <p className="description">{item.description}</p>
+                  </>
+                );
+              })}
+            </div>
+          </section>
+          <section className="buttons_section">
+            <p>
+              <FaArrowAltCircleLeft onClick={handleLeftButtonClick} />
+            </p>
+            <p>
+              <FaArrowAltCircleRight onClick={handleRightButtonClick} />
+            </p>
+          </section>
         </div>
       </div>
     </>
